56df959836159107143e944f2e3ac0a8f314d89f
cerqiest <66092565+cerqiest@users.noreply.github.com>
2023-04-02 14:34:40 -0400

    Fix capitalization in kelp.js

--- kelp.js
+++ kelp.js
@@ -170,7 +170,7 @@ kelp.listen = () => {
 
   app.listen(PORT, async () => {
     console.log(
-      `${colorPrim("Kelp")} ${colorSec("Listening on PORT")} ${colorPrim(PORT)}`
+      `${colorPrim("KELP")} ${colorSec("Listening on PORT")} ${colorPrim(PORT)}`
     );
 
     options.OPTIONS.forEach((v) => {
